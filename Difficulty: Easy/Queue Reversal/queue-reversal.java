5b740e7004e526710b3b510aa9a9727d67210a97
Tanuja0318 <165898802+Tanuja0318@users.noreply.github.com>
2025-09-19 11:34:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Queue Reversal/queue-reversal.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseQueue(Queue<Integer> q) {
+        int n=q.size();
+        int[] res=new int[n];
+        for(int i=0;i<n;i++){
+            res[i]=q.poll();
+        }
+        for(int i=n-1;i>=0;i--)
+            q.offer(res[i]);
+    }
+}
