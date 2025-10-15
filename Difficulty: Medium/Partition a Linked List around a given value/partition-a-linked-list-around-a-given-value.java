5d70280a0cdce698323a079fd9481667dad18942
Tanuja0318 <165898802+Tanuja0318@users.noreply.github.com>
2025-10-15 15:32:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partition a Linked List around a given value/partition-a-linked-list-around-a-given-value.java
@@ -0,0 +1,44 @@
+// User function Template for Java
+
+/*class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution {
+    public static Node partition(Node head, int x) {
+        Node p=new Node(0);
+        Node q=new Node(0);
+        Node r=new Node(0);
+        Node np=p;
+        Node nq=q;
+        Node nr=r;
+        while(head!=null){
+            if(head.data<x){
+                np.next=head;
+                np=np.next;
+            }
+            else if(head.data==x){
+                nq.next=head;
+                nq=nq.next;
+            }
+            else{
+                nr.next=head;
+                nr=nr.next;
+            }
+            head=head.next;
+        }
+        nr.next = null;  
+        np.next = q.next;   
+        nq.next = r.next;   
+        return p.next;
+    }
+}
